19dd94cecd92e8d1d1cb3a807b39369f9c836882
Gordin <9ordin@googlemail.com>
2012-07-03 22:44:51 +0200

    fixed parsing of stream:stream tags

--- src/core.js
+++ src/core.js
@@ -3786,6 +3786,7 @@ Strophe.Websocket.prototype = {
             return;
         }
         var string = message.data.replace(/^<stream:([a-z]*)>/, "<stream:$1 xmlns:stream='http://etherx.jabber.org/streams'>");
+        string = string.replace(/^<stream:stream (.*[^/])>/, "<stream:stream $1/>");
 
         parser = new DOMParser();
         var elem = parser.parseFromString(string, "text/xml").documentElement;
@@ -3800,6 +3801,7 @@ Strophe.Websocket.prototype = {
      */
     _connect_cb_wrapper: function(message) {
         var string = message.data.replace(/^<stream:([a-z]*)>/, "<stream:$1 xmlns:stream='http://etherx.jabber.org/streams'>");;
+        string = string.replace(/^<stream:stream (.*[^/])>/, "<stream:stream $1/>");
 
         var parser = new DOMParser();
         var elem = parser.parseFromString(string, "text/xml").documentElement;
